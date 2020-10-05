8bf571d903bb06db651aeca1c3fbac1dd750067f
Denisse Montalvo <adc.logix@gmail.com>
2020-10-05 08:00:21 -0500

    datos con id

---
+++ app.js
@@ -0,0 +1,131 @@
+const formulario = document.getElementById("formulario");
+// 
+const inputNro = document.getElementById('inputNro') 
+const inputRuc = document.getElementById('inputRuc')
+const inputFecha = document.getElementById('inputFecha')
+const inputNombreEmpresa = document.getElementById('inputNombreEmpresa')
+const inputDireccion = document.getElementById('inputDireccion');
+
+// Detalle de consumo 
+
+const inputCant = document.getElementById('inputCant')
+const selectDescripcion =document.getElementById('selectDescripcion')
+// const inputDescripcion = document.getElementById('inputDescripcion')
+const inputPreUnit = document.getElementById('inputPreUnit')
+const inputPreTotal = document.getElementById('inputPreTotal')
+
+// Boton formulario 
+const btnFormulario = document.getElementById('btnForm')
+// Boton Detalle de consumo 
+const btnDetalle = document.getElementById('btnDetalle')
+// Llamando a tbody 
+const tbody = document.getElementById('tbody')
+
+let facturas = [];
+let productos =[];
+
+// Re-dibujar tbody
+const reDibujarTbody = () => {
+    tbody.innerHTML ='';
+
+    productos.forEach((producto)=>{
+        let tr = document.createElement("tr");
+        let tdCant = document.createElement("td");
+        tdCant.innerText =producto.cantidad;
+        let tdDescrip = document.createElement("td");
+        tdDescrip.innerText = producto.descripcion;
+        let tdPrUnitario = document.createElement("td");
+        tdPrUnitario.innerText = producto.pUnitario;
+        let tdPrTotal = document.createElement("td");
+        tdPrTotal.innerText = producto.pTotal;
+
+        let tdAcciones = document.createElement("td");
+        let btnEliminar = document.createElement("button");
+        btnEliminar.innerText = "Eliminar";
+        // Evento onClick
+        btnEliminar.onclick = () => {
+
+        }
+
+        tdAcciones.appendChild(btnEliminar);
+
+        tr.appendChild(tdCant);
+        tr.appendChild(tdDescrip);
+        tr.appendChild(tdPrUnitario);
+        tr.appendChild(tdPrTotal);
+        tr.appendChild(tdAcciones);
+        tbody.appendChild(tr);
+    });
+
+}
+// Funcion para agregar producto
+const agregarProducto = () => {
+    let objProducto = {
+    cantidad: +inputCant.value,
+    descripcion: selectDescripcion.value ,
+    pUnitario: +inputPreUnit.value, 
+    pTotal: +inputCant.value*inputPreUnit.value,
+    }
+    // console.log(objProducto);
+    productos.push(objProducto);
+    console.log(productos);
+}
+/** Seleciona producto */
+// Funcion Llenar Precio unitario
+const llenarPrecioPorDesc = (id) => {
+    const precioFiltrado = data.filter((dato) => {
+        if(id === dato.id){
+            return  dato;
+        }
+    })
+    // console.log(precioFiltrado);
+    precioFiltrado.forEach((dato)=>{
+        console.log(dato.costo);
+    })
+
+} 
+// Funcion LLenar descripcion de PRODUCTO 
+const seleccionaProducto = () => {
+    let options = `<option value="0"> --Seleccione-- </option>`
+    data.forEach((dato) => {
+        options += `<option value="${dato.id}"> ${dato.descripcion}</option>`
+    });
+    selectDescripcion.innerHTML = options;
+   
+}
+seleccionaProducto();
+
+selectDescripcion.onchange = () => {
+    let id = +selectDescripcion.value;
+    // console.log(id);
+    llenarPrecioPorDesc(id);
+
+}
+
+btnDetalle.onclick = (evento) => {
+    evento.preventDefault();
+    console.log("clic");
+    agregarProducto();
+    reDibujarTbody();
+}
+
+formulario.onsubmit = (evento) => {
+
+    evento.preventDefault();
+    // console.log('Probando formulario ....');
+    
+    let objFactura = {
+        numero: inputNro.value,
+        numeroRuc: inputRuc.value,
+        fecha: inputFecha.value,
+        nombre:  inputNombreEmpresa.value,
+        direcion:  inputDireccion.value,
+    }
+    // console.log(objFactura);
+    facturas.push(objFactura);
+    console.log(`Informacion defacturas`);
+    console.log(facturas);
+}
+
+
+
---
+++ database.js
@@ -0,0 +1,22 @@
+data = [
+    {
+        id: 1, 
+        descripcion:"Capacimetro Digital YU FUNG YF-150",
+        costo: 165,
+    },
+    {
+        id: 2, 
+        descripcion:"Decibelimetro digital TENMARS TM101",
+        costo: 350,
+    },
+    {
+        id: 3, 
+        descripcion:"Medidior de Distancia Laser UNI-T",
+        costo: 220,
+    },
+    {
+        id: 4, 
+        descripcion:"Multimetro Digital SANWA S-75",
+        costo: 70,
+    }
+]
